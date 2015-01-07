ce00b90fd92d1e4f07da574e3ab01e4ef3155129
xhs <chef@dark.kitchen>
2015-01-07 15:08:32 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='WebPocket',
+      version='0.1.0',
+      description='Actor-model based WebSocket client in Python',
+      long_description='Actor-model based WebSocket client in Python',
+      author='xhs',
+      author_email='chef@dark.kitchen',
+      url='https://github.com/xhs/WebPocket/',
+      packages=['webpocket'],
+      platforms = ["any"],
+      license = 'BSD')
