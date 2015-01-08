8d09accdd108400ca8d1abc778cbf9e176f034eb
xhs <chef@dark.kitchen>
2015-01-09 03:19:05 +0800

    add fragment support

--- webpocket/framer.py
+++ webpocket/framer.py
@@ -5,17 +5,37 @@ from frame import *
 from exc import *
 
 class Framer(object):
-  def __init__(self, actors):
+  def __init__(self, actors, payload_limit=0):
     self._actors = actors
     self._buffer = b''
+    self._payload_limit = payload_limit
 
   def send_text(self, text):
-    data = Frame(opcode=OPCODE_TEXT, payload=text).encode()
-    self._actors['streamer'].send(data)
+    limit = self._payload_limit
+    if limit > 0 and limit < len(text):
+      text = self._send_fragment(0, OPCODE_TEXT, text, limit)
+      while limit < len(text):
+        text = self._send_fragment(0, OPCODE_CONTINUE, text, limit)
+      self._send_fragment(1, OPCODE_CONTINUE, text, limit)
+    else:
+      data = Frame(opcode=OPCODE_TEXT, payload=text).encode()
+      self._actors['streamer'].send(data)
 
   def send_binary(self, binary):
-    data = Frame(opcode=OPCODE_BINARY, payload=binary).encode()
+    limit = self._payload_limit
+    if limit > 0 and limit < len(binary):
+      binary = self._send_fragment(0, OPCODE_BINARY, binary, limit)
+      while limit < len(binary):
+        binary = self._send_fragment(0, OPCODE_CONTINUE, binary, limit)
+      self._send_fragment(1, OPCODE_CONTINUE, binary, limit)
+    else:
+      data = Frame(opcode=OPCODE_TEXT, payload=text).encode()
+      self._actors['streamer'].send(data)
+
+  def _send_fragment(self, fin, opcode, payload, limit):
+    data = Frame(fin=fin, opcode=opcode, payload=payload[:limit]).encode()
     self._actors['streamer'].send(data)
+    return payload[limit:]
 
   def feed(self, data):
     self._buffer += data
